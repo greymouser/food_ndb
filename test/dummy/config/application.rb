d8c94ac436d6e6abe4d11e2d41e9f0e357ae5cf2
Armando Di Cianno <armando@dicianno.org>
2013-08-23 15:52:19 -0400

    Incorrect path when moved to rails 4.0.0 from an example app

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 Bundler.require(*Rails.groups)
-require "test_engine"
+require "food_ndb"
 
 module Dummy
   class Application < Rails::Application
