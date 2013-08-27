652719870c448d86d03c098de1897c0e6f85a9ba
Armando Di Cianno <armando@dicianno.org>
2013-08-27 09:47:44 -0400

    Cleaned up compound and composite keys, just in case I need to go there.

--- db/migrate/20130216224003_sr25_schema.rb
+++ db/migrate/20130216224003_sr25_schema.rb
@@ -1,69 +1,7 @@
 class Sr25Schema < ActiveRecord::Migration
   def change
-    create_table :food_ndb_data_sources, id: false, primary_key: :id do |t|
-      t.string :id,             limit: 6,   :null => false
-      t.string :authors,        limit: 255
-      t.string :title,          limit: 255, :null => false
-      t.string :year,           limit: 4
-      t.string :journal,        limit: 135
-      t.string :volume_or_city, limit: 16
-      t.string :issue_or_state, limit: 5
-      t.string :start_page,     limit: 5
-      t.string :end_page,       limit: 5
-    end
-    add_index :food_ndb_data_sources, :id, unique: true
 
-
-    create_table :food_ndb_derivations, id: false, primary_key: :code do |t|
-      t.string :code,        limit: 4,   null: false
-      t.string :description, limit: 120, null: false
-    end
-    add_index :food_ndb_derivations, :code, unique: true
-
-
-    create_table :food_ndb_food_groups, id: false, primary_key: :code do |t|
-      t.integer :code,        limit: 2,  null: false
-      t.string  :description, limit: 60, null: false
-    end
-    add_index :food_ndb_food_groups, :code, :unique => true
-
-
-    create_table :food_ndb_food_nutrients, id: false do |t|
-      t.integer :nutrient_databank_number,           limit: 3,                          null: false
-      t.integer :nutrient_number,                    limit: 3,                          null: false
-      t.decimal :value,                                        precision: 13, scale: 3, null: false
-      t.decimal :points_of_data,                               precision: 5,  scale: 0, null: false
-      t.decimal :standard_error,                               precision: 11, scale: 3
-      t.string  :source_code,                        limit: 2,                          null: false
-      t.string  :derivation_code,                    limit: 4
-      t.string  :reference_nutrient_databank_number, limit: 5
-      t.boolean :enriched
-      t.integer :number_of_studies,                  limit: 1
-      t.decimal :min,                                          precision: 13, scale: 3
-      t.decimal :max,                                          precision: 13, scale: 3
-      t.integer :degrees_of_freedom,                 limit: 2
-      t.decimal :lower_error_bound,                            precision: 13, scale: 3
-      t.decimal :upper_error_bound,                            precision: 13, scale: 3
-      t.string  :statistical_comments,               limit: 10
-      t.string  :added_or_modified_date,             limit: 10
-    end
-    add_index :food_ndb_food_nutrients,
-              [:nutrient_databank_number, :nutrient_number],
-              :name => 'by_food_and_nutrient'
-    add_index :food_ndb_food_nutrients, :derivation_code
-    add_index :food_ndb_food_nutrients, :source_code
-
-
-    create_table :food_ndb_food_nutrients_data_sources, id: false do |t|
-      t.integer :nutrient_databank_number, limit: 3, null: false
-      t.integer :nutrient_number,          limit: 3, null: false
-      t.string  :data_source_id,           limit: 6, null: false
-    end
-    add_index :food_ndb_food_nutrients_data_sources,
-              [:nutrient_databank_number, :nutrient_number, :data_source_id],
-              :name => 'by_food_nutrient_and_data_source'
-
-
+    ###################################################################
     create_table :food_ndb_foods, id: false, primary_key: :nutrient_databank_number do |t|
       t.integer :nutrient_databank_number, limit: 3,                              null: false
       t.integer :food_group_code,          limit: 2,                              null: false
@@ -84,28 +22,25 @@ class Sr25Schema < ActiveRecord::Migration
     add_index :food_ndb_foods, :food_group_code
 
 
+    ###################################################################
+    create_table :food_ndb_food_groups, id: false, primary_key: :code do |t|
+      t.integer :code,        limit: 2,  null: false
+      t.string  :description, limit: 60, null: false
+    end
+    add_index :food_ndb_food_groups, :code, :unique => true
+
+
+    ###################################################################
     create_table :food_ndb_foods_languals, id: false do |t|
       t.integer :nutrient_databank_number, limit: 3, null: false
       t.string  :langual_code,             limit: 5, null: false
     end
     add_index :food_ndb_foods_languals,
               [:nutrient_databank_number, :langual_code],
-              :name => 'by_ndbn_and_langual',
-              unique: true
+                :name => 'by_ndbn_and_langual',
+                unique: true
 
-
-    create_table :food_ndb_footnotes, id: false do |t|
-      t.integer :nutrient_databank_number, limit: 3,   null: false
-      t.string  :sequence,                 limit: 4,   null: false
-      t.string  :the_type,                 limit: 1,   null: false
-      t.integer :nutrient_number,          limit: 3
-      t.string  :the_text,                 limit: 200, null: false
-    end
-    add_index :food_ndb_footnotes,
-              [:nutrient_databank_number, :sequence, :the_type],
-              name: 'by_food_sequence_and_the_type'
-
-
+    ###################################################################
     create_table :food_ndb_languals, id: false, primary_key: :code do |t|
       t.string :code,        limit: 5,   null: false
       t.string :description, limit: 140, null: false
@@ -113,6 +48,34 @@ class Sr25Schema < ActiveRecord::Migration
     add_index :food_ndb_languals, :code, unique: true
 
 
+    ###################################################################
+    create_table :food_ndb_food_nutrients, id: false do |t|
+      t.integer :nutrient_databank_number,           limit: 3,                          null: false
+      t.integer :nutrient_number,                    limit: 3,                          null: false
+      t.decimal :value,                                        precision: 13, scale: 3, null: false
+      t.decimal :points_of_data,                               precision: 5,  scale: 0, null: false
+      t.decimal :standard_error,                               precision: 11, scale: 3
+      t.string  :source_code,                        limit: 2,                          null: false
+      t.string  :derivation_code,                    limit: 4
+      t.string  :reference_nutrient_databank_number, limit: 5
+      t.boolean :enriched
+      t.integer :number_of_studies,                  limit: 1
+      t.decimal :min,                                          precision: 13, scale: 3
+      t.decimal :max,                                          precision: 13, scale: 3
+      t.integer :degrees_of_freedom,                 limit: 2
+      t.decimal :lower_error_bound,                            precision: 13, scale: 3
+      t.decimal :upper_error_bound,                            precision: 13, scale: 3
+      t.string  :statistical_comments,               limit: 10
+      t.string  :added_or_modified_date,             limit: 10
+    end
+    add_index :food_ndb_food_nutrients,
+                [:nutrient_databank_number, :nutrient_number],
+                :name => 'by_food_and_nutrient', unique: true
+    add_index :food_ndb_food_nutrients, :derivation_code
+    add_index :food_ndb_food_nutrients, :source_code
+
+
+    ###################################################################
     create_table :food_ndb_nutrients, :id => false, primary_key: :number do |t|
       t.integer :number,                 limit: 3,  null: false
       t.string  :units,                  limit: 7,  null: false
@@ -124,6 +87,7 @@ class Sr25Schema < ActiveRecord::Migration
     add_index :food_ndb_nutrients, :number, unique: true
 
 
+    ###################################################################
     create_table :food_ndb_sources, id: false, primary_key: :code do |t|
       t.integer :code,        limit: 1,  null: false
       t.string  :description, limit: 60, null: false
@@ -131,6 +95,15 @@ class Sr25Schema < ActiveRecord::Migration
     add_index :food_ndb_sources, :code, unique: true
 
 
+    ###################################################################
+    create_table :food_ndb_derivations, id: false, primary_key: :code do |t|
+      t.string :code,        limit: 4,   null: false
+      t.string :description, limit: 120, null: false
+    end
+    add_index :food_ndb_derivations, :code, unique: true
+
+
+    ###################################################################
     create_table :food_ndb_weights, id: false do |t|
       t.integer :nutrient_databank_number, limit: 3,                null: false
       t.string  :sequence,                 limit: 2,                null: false
@@ -141,6 +114,53 @@ class Sr25Schema < ActiveRecord::Migration
       t.decimal :standard_deviation,       precision: 10, scale: 3
     end
     add_index :food_ndb_weights,
-              [:nutrient_databank_number, :sequence]
+              [:nutrient_databank_number, :sequence],
+              unique: true
+
+
+    ###################################################################
+    create_table :food_ndb_footnotes, id: false do |t|
+      t.integer :nutrient_databank_number, limit: 3,   null: false
+      t.string  :sequence,                 limit: 4,   null: false
+      t.string  :the_type,                 limit: 1,   null: false
+      t.integer :nutrient_number,          limit: 3
+      t.string  :the_text,                 limit: 200, null: false
+    end
+    # Cannot index --
+    # According to sr26.pdf, sequence can apply to more than 1
+    #   nutrient_number per nutrient_databank_number; furthermore
+    #   nutrient_number may be null
+    add_index :food_ndb_footnotes,
+              [:nutrient_databank_number, :sequence],
+              name: 'by_food_and_sequence'
+
+
+    ###################################################################
+    create_table :food_ndb_food_nutrients_data_sources, id: false do |t|
+      t.integer :nutrient_databank_number, limit: 3, null: false
+      t.integer :nutrient_number,          limit: 3, null: false
+      t.string  :data_source_id,           limit: 6, null: false
+    end
+    add_index :food_ndb_food_nutrients_data_sources,
+              [:nutrient_databank_number, :nutrient_number, :data_source_id],
+              :name => 'by_food_nutrient_and_data_source',
+              unique: true
+
+
+    ###################################################################
+    create_table :food_ndb_data_sources, id: false, primary_key: :id do |t|
+      t.string :id,             limit: 6,   :null => false
+      t.string :authors,        limit: 255
+      t.string :title,          limit: 255, :null => false
+      t.string :year,           limit: 4
+      t.string :journal,        limit: 135
+      t.string :volume_or_city, limit: 16
+      t.string :issue_or_state, limit: 5
+      t.string :start_page,     limit: 5
+      t.string :end_page,       limit: 5
+    end
+    add_index :food_ndb_data_sources, :id, unique: true
+
+
   end
 end
